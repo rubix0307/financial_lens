a066599e96075e2c6e6f5613c001c435a931e0e8
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-10-20 23:37:31 +0200

    added get_paginated_receipts as section service and use it for temp main section page

--- section/views.py
+++ section/views.py
@@ -1,3 +1,15 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .services.feed import get_paginated_receipts
+
+
+def index(request: WSGIRequest) -> HttpResponse:
+    context = {
+        'page_obj': get_paginated_receipts(section_id=1, page=request.GET.get('page', 1), per_page=8)
+    }
+    return render(request, 'section/index.html', context=context)
+
+def get_feed(request: WSGIRequest) -> HttpResponse:
+    return render(request, 'section/index.html')
