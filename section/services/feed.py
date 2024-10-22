6612fa3cd614c35a5f72d22161ea7a3fa4dbc7cb
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-10-22 20:25:50 +0200

    added get_paginated_receipts with tests

--- section/services/feed.py
+++ section/services/feed.py
@@ -1,7 +1,12 @@
-from receipt import services as r_services
+from common.services.paginator import get_paginated_objects
+from receipt.models import Receipt
 
 
-def get_paginated_receipts(section_id, page=1, per_page=10) -> r_services.get_paginated_receipts:
-    page_obj = r_services.get_paginated_receipts(section_id, page, per_page)
-    page_obj.object_list = page_obj.object_list.prefetch_related('products')
-    return page_obj
+def get_paginated_receipts(section_id, page=1, per_page=10) -> get_paginated_objects:
+    receipts = (Receipt.objects
+                .filter(section_id=section_id)
+                .order_by('-date', '-id')
+                .prefetch_related('products')
+                )
+
+    return get_paginated_objects(receipts, page=page, per_page=per_page)
